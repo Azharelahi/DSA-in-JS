5e4134c180bb99866771853913b01154849bcea5
Azharelahi <143085056+Azharelahi@users.noreply.github.com>
2025-10-06 03:13:49 +0500

    Array of objects are implemente

---
+++ Objects/object.js
@@ -0,0 +1,8 @@
+let students = [
+  { name: "Azhar Elahi", Roll_no: 1, age: 23 },
+  { name: "Mazhar Elahi", Roll_no: 2, age: 22 },
+];
+console.log(students);
+
+console.log(students[1].Roll_no);
+console.log(students[0].name, " ", students[0].Roll_no);
