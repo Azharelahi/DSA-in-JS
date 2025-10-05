7be1b303abe2706b4d2b9b481b8f61f0912f68d8
Azharelahi <143085056+Azharelahi@users.noreply.github.com>
2025-10-06 03:19:29 +0500

    logic implemented on the objects for search using for loop and conditions

--- Objects/object.js
+++ Objects/object.js
@@ -1,8 +1,30 @@
 let students = [
   { name: "Azhar Elahi", Roll_no: 1, age: 23 },
   { name: "Mazhar Elahi", Roll_no: 2, age: 22 },
+  { name: "Hammad Khan", Roll_no: 3, age: 24 },
+  { name: "Ahsan Malik", Roll_no: 4, age: 21 },
+  { name: "Saad Ahmed", Roll_no: 5, age: 23 },
+  { name: "Bilal Hussain", Roll_no: 6, age: 22 },
+  { name: "Umar Farooq", Roll_no: 7, age: 24 },
+  { name: "Fahad Ali", Roll_no: 8, age: 25 },
+  { name: "Suleman Tariq", Roll_no: 9, age: 23 },
+  { name: "Ahmed Raza", Roll_no: 10, age: 22 },
+  { name: "Talha Javed", Roll_no: 11, age: 21 },
+  { name: "Usman Khalid", Roll_no: 12, age: 23 },
+  { name: "Hamza Aslam", Roll_no: 13, age: 24 },
+  { name: "Waleed Shah", Roll_no: 14, age: 22 },
+  { name: "Rehan Qureshi", Roll_no: 15, age: 23 },
+  { name: "Adil Zaman", Roll_no: 16, age: 25 },
+  { name: "Imran Yousaf", Roll_no: 17, age: 21 },
+  { name: "Danish Khan", Roll_no: 18, age: 22 },
+  { name: "Asad Mehmood", Roll_no: 19, age: 23 },
+  { name: "Noman Arif", Roll_no: 20, age: 24 },
 ];
-console.log(students);
 
-console.log(students[1].Roll_no);
-console.log(students[0].name, " ", students[0].Roll_no);
+console.log(students);
+let user_name = "Ahsan Malik";
+for (let i = 0; i < students.length; i++) {
+  if (students[i].name == user_name) {
+    console.log(students[i]);
+  }
+}
